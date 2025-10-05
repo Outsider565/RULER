e38e3b9a8c43167f36648a2da525453a1e09fcd5
shaowenwang <shaowenwang@tencent.com>
2025-10-06 02:28:26 +0800

    More tokens to generate

--- scripts/data/synthetic/constants.py
+++ scripts/data/synthetic/constants.py
@@ -29,25 +29,25 @@ TASKS = {
     },
     
     'variable_tracking': {
-        'tokens_to_generate': 30,
+        'tokens_to_generate': 128,
         'template': """Memorize and track the chain(s) of variable assignment hidden in the following text.\n\n{context}\nQuestion: Find all variables that are assigned the value {query} in the text above.""",
         'answer_prefix': """ Answer: According to the chain(s) of variable assignment in the text above, {num_v} variables are assigned the value {query}, they are: """
     },
     
     'common_words_extraction': {
-        'tokens_to_generate': 120,
+        'tokens_to_generate': 128,
         'template': """Below is a numbered list of words. In these words, some appear more often than others. Memorize the ones that appear most often.\n{context}\nQuestion: What are the 10 most common words in the above list?""",
         'answer_prefix': """ Answer: The top 10 words that appear most often in the list are:"""
     },
     
     'freq_words_extraction' : {
-        'tokens_to_generate': 50,
+        'tokens_to_generate': 128,
         'template': """Read the following coded text and track the frequency of each coded word. Find the three most frequently appeared coded words. {context}\nQuestion: Do not provide any explanation. Please ignore the dots '....'. What are the three most frequently appeared words in the above coded text?""",
         'answer_prefix': """ Answer: According to the coded text above, the three most frequently appeared words are:"""
     },
 
     'qa': {
-        'tokens_to_generate': 32, 
+        'tokens_to_generate': 128, 
         'template': """Answer the question based on the given documents. Only give me the answer and do not output any other words.\n\nThe following are given documents.\n\n{context}\n\nAnswer the question based on the given documents. Only give me the answer and do not output any other words.\n\nQuestion: {query}""",
         'answer_prefix': """ Answer:""",
     },
